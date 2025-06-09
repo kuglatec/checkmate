559ed1dc3ea50ffb8a0c55374ca1c0a168b7b897
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-06-09 20:50:34 +0200

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,6 +1,23 @@
 struct Position {
 char player; //The player to make his move. White = 0, Black = 1
-char wcastle;// Castle right for white. 0 = no castling, 1 = kingside, 2 = queenside 3 = both.
-char bcastle;// same but for black
+char wcastle;// Castle rights for white. 0 = no castling, 1 = kingside, 2 = queenside 3 = both.
+char bcastle;// same for black
 char board[8][8];
+};
+
+struct Move {
+    char start[2];
+    char end[2];
+};
+
+struct Square {
+    char x;
+    char y;
+};
+
+struct Node {
+    struct Position position;
+    struct Move move;
+    int nchildren;
+    struct Node *children;
 };
