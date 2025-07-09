98e3b3f0d9815bdbe82f903e96102c0ed6df2ef3
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-07-09 15:53:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,12 @@
 int main () {
     struct Position pos = FENtoPosition(FEN);
     ApplyCastlingRights(FEN, &pos);
-    printPosition(pos)rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1;
-    printf("\n:%d", pos.wcastle);
+    printPosition(pos);
+    struct Move myMove;
+    myMove.start[0] = 1;
+    myMove.start[1] = 1;
+    myMove.end[0] = 6;
+    myMove.end[1] = 2;
+    int v = validityCheck(pos, myMove);
     return 0;
 }
