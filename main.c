f137106726bc2c995cd7819d3be43551cb5ac2b7
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-07-18 01:57:58 +0200

    Update main.c
    this is just testing code

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include "engine.c"
-#define FEN "8/8/8/8/1r6/P7/8/8 w - - 0 1"
+#define FEN "8/8/8/3Q4/8/8/8/8 w - - 0 1"
 int main () {
     struct Position pos = FENtoPosition(FEN);
     ApplyCastlingRights(FEN, &pos);
-//    printPosition(pos);
-    struct Move myMove;
-    myMove.start[0] = 0;
-    myMove.start[1] = 2;
-    myMove.end[0] = 1;
-    myMove.end[1] = 3;
-    int v = validityCheck(pos, myMove);
-    printf("\nresult: %d", v);
+    //printPosition(pos);
+    /*
+    struct Move mymove;
+    mymove.start.x = 2;
+    mymove.start.y = 3;
+    mymove.end.x = 5;
+    mymove.end.y = 0;
+    */
+    //printf("\nValid: %d\n", validityCheck(pos, mymove));
+    int counter = 0;
+    struct Move* moves = getMoves(pos, &counter);
+    printf("\ncounter: %d", counter);
+   
     return 0;
 }
