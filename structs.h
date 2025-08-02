0754f559a834709c58c9c978c178185962480542
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-08-03 00:21:20 +0200

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,23 +1,30 @@
 #include <stddef.h>
-struct Position {
-int player; //The player to make his move. Black = 0, White = 1
-int wcastle; // Castle rights for white. 0 = no castling, 1 = kingside, 2 = queenside 3 = both.
-int bcastle; // same for black
-char board[8][8];
-};
-
 
 struct Square {
     char x;
     char y;
 };
 
+struct enPassant {
+    int valid; 
+    struct Square square; 
+};
+
+struct Position {
+int player; //The player to make his move. Black = 0, White = 1
+int wcastle; // Castle rights for white. 0 = no castling, 1 = kingside, 2 = queenside 3 = both.
+int bcastle; // same for black
+char board[8][8];
+struct enPassant enpassant; //e.p. capture square
+};
+
+
+
 struct Move {
     struct Square start;
     struct Square end;
-    int promotes;
-    char promotion; // Defines what the piece promotes to 
-    int is_castling;
+    int promotes; // 1 = yes, 0 = no
+    char promotion; // Defines what the piece promotes to by char
 };
 
 struct Node {
