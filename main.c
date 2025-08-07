4ec10cb9a411c3bdba7b1e2f4b7b7512b9bcb25f
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-08-07 17:10:07 +0200

    see engine.c commit

--- main.c
+++ main.c
@@ -1,14 +1,35 @@
 #include "engine.c"
 #include <stdio.h>
 #define FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-//#define FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+/*
 int main () {
   struct Position pos = FENtoPosition(FEN);
   ApplyCastlingRights(FEN, &pos);
-  struct Node node;
+  ApplyEnpassantTarget(FEN, &pos);
+    struct Node node;
   node.position = pos;
   buildTree(&node, 5);
-  printf("\nTree built with %d children\n", node.children[0].nchildren);
-  printPosition(node.children[0].children[0].children[0].position);
+ // printf("\nTree built with %d children\n", node.children[0].nchildren;
+
+printTree(node, 5);
+  return 0;
+}
+*/
+int main() {
+  initZobrist();
+
+  struct Position pos = FENtoPosition(FEN);
+  ApplyCastlingRights(FEN, &pos);
+  ApplyEnpassantTarget(FEN, &pos);
+
+  struct hashSet* seen = createHashSet(10000); // Start with space for 10k positions
+
+  struct Node node;
+  node.position = pos;
+  buildTreeWithHashCheck(&node, 6, seen);
+
+  printf("Total unique positions seen: %zu\n", seen->size);
+
+  freeHashSet(seen);
   return 0;
 }
