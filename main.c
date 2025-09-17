7266a27c609efb094fc62c5854cfadd1577e6c76
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2025-09-17 17:11:12 +0200

    simplified main uci interface

--- main.c
+++ main.c
@@ -1,139 +1,91 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include "structs.h"
 #include "engine.c"
-#define LINE_MAX 1024
-#define DEFAULT_FEN "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 
-static void handle_uci() {
-    printf("id name checkmate\n");
-    printf("id author kuglatec\n");
-    printf("uciok\n");
-    fflush(stdout);
-}
-
-static void handle_isready() {
-    printf("readyok\n");
-    fflush(stdout);
-}
-
-static void handle_position(const char *args, struct Position *position) {
-    if (strstr(args, "startpos") == args) {
-        *position = FENtoPosition(DEFAULT_FEN);
-        ApplyCastlingRights(DEFAULT_FEN, position);
-        ApplyEnpassantTarget(DEFAULT_FEN, position);
-        const char *moves = strstr(args, "moves");
-        if (moves) {
-            moves += strlen("moves");
-            while (*moves == ' ') moves++;
-            if (*moves) {
-                      int sc = 1;
-                      struct Move* mvs = notationsToMoves(moves, strlen(moves), &sc);
-                      for (int i = 0; i < sc; i++) {
-
-                          makeMove(position, mvs[i], getSpecialMoveType(position, mvs[i]));
-                      }
-                    free(mvs);
-            }
-        }
-
-    } else if (strncmp(args, "fen", 3) == 0) {
-        const char *moves = strstr(args, "moves");
-        if (moves) {
-            size_t fenlen = (size_t)(moves - args);
-            char *fen = malloc(fenlen + 1);
-            if (fen) {
-                strncpy(fen, args, fenlen);
-                fen[fenlen] = '\0';
-                *position = FENtoPosition(fen + 4);
-                ApplyCastlingRights(fen, position);
-                ApplyEnpassantTarget(fen, position);
-                free(fen);
-            }
-            moves += strlen("moves");
-            while (*moves == ' ') moves++;
-            int sc = 1;
-            struct Move* mvs = notationsToMoves(moves, strlen(moves), &sc);
-            for (int i = 0; i < sc; i++) {
-                makeMove(position, mvs[i], getSpecialMoveType(position, mvs[i]));
-
-            }
-            free(mvs);
-        } else {
-            *position = FENtoPosition(args + 4);
-            ApplyCastlingRights(args + 4, position);
-            ApplyEnpassantTarget(args + 4, position);
-        }
-    }
-    fflush(stdout);
-}
+int main() {
+    init_zobrist();
+    struct HashTable* hash_table = createHashTable(1024);
 
 
-static void handle_go(const char *args, struct Position *pos) {
-    (void)args;
-  //  printf("\nhash: %lu\n", hash_position(pos));
-
-  //  struct Move bestmove = getBestMoveNegamax(pos, 5);
-    //printPosition(*pos);
-    struct HashTable* ht = createHashTable(1024); //1gb table size
-    struct Move bestmove = /*getBestMoveWithHash(pos, 6, ht);*/getBestMoveWithKillers(pos, 8, ht);
-    printf("\nhas zugzwang: %d\n", hasZugzwang(*pos));
-    //struct Move bestmove = getBestMoveWithHash(pos, 5, hashTable);
-
-    if (bestmove.start.x == -1) {
-        printf("bestmove 0000\n");
-    } else {
-        printf("bestmove %c%d%c%d",
-               bestmove.start.x + 'a', bestmove.start.y + 1,
-               bestmove.end.x + 'a', bestmove.end.y + 1);
-
-        if (bestmove.promotes) {
-            printf("%c", bestmove.promotion);
-        }
-        printf("\n");
-    }
-    fflush(stdout);
-}
-
-int main(void) {
+    char fen[90];
     struct Position position;
-    char line[LINE_MAX];
-    printf("Ready to accept UCI commands.\n");
-    fflush(stdout);
 
-    while (fgets(line, sizeof(line), stdin)) {
-        size_t len = strlen(line);
-        if (len && (line[len-1] == '\n' || line[len-1] == '\r')) {
-            while (len && (line[len-1] == '\n' || line[len-1] == '\r')) {
-                line[--len] = '\0';
-            }
-        }
-        if (len == 0) continue;
+    //starting pos
+    strcpy(fen, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    position = FENtoPosition(fen);
+    ApplyCastlingRights(fen, &position);
+    ApplyEnpassantTarget(fen, &position);
 
-        if (strcmp(line, "uci") == 0) {
-            handle_uci();
-        } else if (strcmp(line, "isready") == 0) {
-            handle_isready();
-        } else if (strcmp(line, "ucinewgame") == 0) {
 
-        } else if (strncmp(line, "setoption", 9) == 0) {
+    char buffer[2048];
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stdin, buffer, _IOLBF, 2048);
 
-        } else if (strncmp(line, "position", 8) == 0) {
-            const char *args = line + 8;
-            while (*args == ' ') args++;
-            handle_position(args, &position);
-        } else if (strncmp(line, "go", 2) == 0) {
-            const char *args = line + 2;
-            while (*args == ' ') args++;
-            handle_go(args, &position);
-        } else if (strcmp(line, "quit") == 0) {
+    char* line = NULL;
+    size_t len = 0;
+
+
+    while (1) {
+        if (getline(&line, &len, stdin) == -1) {
             break;
-        } else {
-            printf("info unknown command: %s\n", line);
-            fflush(stdout);
+        }
+
+        if (strncmp(line, "uci", 3) == 0) {
+            printf("id name chess2\n");
+            printf("id author kugla\n");
+            printf("uciok\n");
+        } else if (strncmp(line, "isready", 7) == 0) {
+            printf("readyok\n");
+
+        } else if (strncmp(line, "ppos", 4) == 0)
+        {
+            printPosition(position);
+        }
+
+        else if (strncmp(line, "ucinewgame", 10) == 0) {
+            clearHashTable(hash_table);
+            strcpy(fen, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+            position = FENtoPosition(fen);
+            ApplyCastlingRights(fen, &position);
+            ApplyEnpassantTarget(fen, &position);
+        } else if (strncmp(line, "position", 8) == 0) {
+            if (strstr(line, "startpos")) {
+                strcpy(fen, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+                position = FENtoPosition(fen);
+                ApplyCastlingRights(fen, &position);
+                ApplyEnpassantTarget(fen, &position);
+            } else if (strstr(line, "fen")) {
+                char* fen_ptr = strstr(line, "fen") + 4;
+                strcpy(fen, fen_ptr);
+                position = FENtoPosition(fen);
+                ApplyCastlingRights(fen, &position);
+                ApplyEnpassantTarget(fen, &position);
+            }
+
+            if (strstr(line, "moves")) {
+                char* moves_ptr = strstr(line, "moves") + 6;
+                int move_count;
+                struct Move* moves = notationsToMoves(moves_ptr, strlen(moves_ptr), &move_count);
+                for (int i = 0; i < move_count; i++) {
+                    makeMove(&position, moves[i], getSpecialMoveType(&position, moves[i]));
+                }
+            }
+        } else if (strncmp(line, "go", 2) == 0) {
+            struct Move best_move = getBestMoveWithKillers(&position, 8, hash_table);
+            char promo_char = best_move.promotes ? best_move.promotion : ' ';
+            if (promo_char != ' ') {
+              printf("bestmove %c%d%c%d%c\n", best_move.start.x + 'a', best_move.start.y + 1, best_move.end.x + 'a', best_move.end.y + 1, promo_char);
+            } else {
+              printf("bestmove %c%d%c%d\n", best_move.start.x + 'a', best_move.start.y + 1, best_move.end.x + 'a', best_move.end.y + 1);
+            }
+        } else if (strncmp(line, "quit", 4) == 0) {
+            break;
         }
     }
 
+    free(line);
+    freeHashTable(hash_table);
     return 0;
 }
